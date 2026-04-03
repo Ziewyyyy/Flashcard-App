80115b167b7961d8c79d8033e50edce2dbfa90a0
Ziewyyyy <trinhanhc5@gmail.com>
2026-04-03 23:53:01 +0700

    Update

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class Database {
 
-    private static final String URL = "jdbc:sqlite:anki.db";
+    private static final String URL = "jdbc:sqlite:fc.db";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL);
